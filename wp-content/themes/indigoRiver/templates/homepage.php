bf1b8bb674acd217afc145812ef2047df6f73b51
Chris <chris@indigo-river.com>
2015-08-03 16:49:44 +0100

    Header logos & CSS added

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -3,6 +3,11 @@ Template Name: Home Page
 */ 
 get_header(); ?>
 
+<header>
+<a class="homeLogo" href="http://indigo-river.com/"><img src="<?php bloginfo('template_url');?>/img/IndigoRiver_Logo_White-01.svg"></a>
+
+</header>
+
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
