af2be0cbcebb7b10ab61275b1241ccb9025632e8
Chris <chris@indigo-river.com>
2015-08-03 17:13:37 +0100

    Add sidebar css * structure

--- wp-content/themes/indigoRiver/templates/includes/sidebar_left.php
+++ wp-content/themes/indigoRiver/templates/includes/sidebar_left.php
@@ -1,3 +1,11 @@
 <div id="pageLocator">
-
+	<div class="locatorText">
+    	<p class="animated slideInRight">HELLO</p>
+    </div>
+	<div class="pagination">
+        <div class="locatorLine animated slideInRight" id="LLOne" style="background: rgb(0, 0, 0);"></div>
+        <div class="locatorLine animated slideInRight" id="LLTwo" style="background: rgb(204, 204, 204);"></div>
+        <div class="locatorLine animated slideInRight" id="LLThree" style="background: rgb(204, 204, 204);"></div>
+        <div class="locatorLine animated slideInRight" id="LLFour" style="background: rgb(204, 204, 204);"></div>
+	</div>
 </div>
