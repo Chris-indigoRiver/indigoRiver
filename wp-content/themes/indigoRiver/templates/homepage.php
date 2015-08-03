b225c17db2071b3a091d5faff27f62b8fca4a78c
Chris <chris@indigo-river.com>
2015-08-03 15:53:13 +0100

    updated css & added sidebar

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -5,6 +5,9 @@ get_header(); ?>
 
 <section id="content" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php include('includes/sidebar_left.php'); ?>
+
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
 <?php endwhile; endif; ?>
