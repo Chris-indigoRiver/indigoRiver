375b803ffdb54072ee91efa846ac5ccde499b8a0
Jack-indigoRiver <jack@indigo-river.com>
2015-08-04 12:05:17 +0100

    nav links

--- wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
+++ wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
@@ -4,4 +4,11 @@
         <div class="two"></div>
         <div class="three"></div>
     </div>
+    <div class="nav">
+    	<ul>
+        	<li><h2 class="white">US</h2></li>
+            <li><h2 class="white">WORK</h2></li>
+            <li><h2 class="white">CONTACT</h2></li>
+        </ul>
+    </div>
 </div>
