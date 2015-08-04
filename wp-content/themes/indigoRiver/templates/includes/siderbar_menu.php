45fd0267eca43f9cb5ef9ef92039c42059fc4afb
Jack-indigoRiver <jack@indigo-river.com>
2015-08-04 13:10:00 +0100

    animate css nav

--- wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
+++ wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
@@ -7,8 +7,8 @@
     <div class="nav">
     	<ul>
         	<li><h2 class="white animated fadeInRight">US</h2></li>
-            <li><h2 class="white animated fadeInRight">WORK</h2></li>
-            <li><h2 class="white animated fadeInRight">CONTACT</h2></li>
+            <li><h2 class="white animated fadeInRight delay-zero-five">WORK</h2></li>
+            <li><h2 class="white animated fadeInRight delay-one">CONTACT</h2></li>
         </ul>
         <div class="terms">
         	<small>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</small>
