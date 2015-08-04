122ef6833b1f215ba4cfa4a945d251fd03af7c3f
Jack-indigoRiver <jack@indigo-river.com>
2015-08-04 13:08:17 +0100

    terms edit

--- wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
+++ wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
@@ -6,13 +6,14 @@
     </div>
     <div class="nav">
     	<ul>
-        	<li><h2 class="white animated fadeInLeft">US</h2></li>
-            <li><h2 class="white animated fadeInLeft">WORK</h2></li>
-            <li><h2 class="white animated fadeInLeft">CONTACT</h2></li>
+        	<li><h2 class="white animated fadeInRight">US</h2></li>
+            <li><h2 class="white animated fadeInRight">WORK</h2></li>
+            <li><h2 class="white animated fadeInRight">CONTACT</h2></li>
         </ul>
-        <div class="Terms">
+        <div class="terms">
         	<small>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</small>
-        	<small>indigoRiver 2015 ©</small>
+        	<br>
+            <small>indigoRiver 2015 ©</small>
         </div>
     </div>
 </div>
