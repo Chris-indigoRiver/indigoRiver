af88df395dae825a4d323199ba2dd8c588774543
Chris <chris@indigo-river.com>
2015-08-04 09:52:33 +0100

    Added plugin.js to /js to place custom javascript in

--- wp-content/themes/indigoRiver/header.php
+++ wp-content/themes/indigoRiver/header.php
@@ -10,7 +10,7 @@
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="//use.typekit.net/ozh5zle.js"></script>
-
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/plugin.js"></script>
 
 <?php wp_head(); ?>
 </head>
--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -6,6 +6,8 @@ get_header(); ?>
 <header>
 <a class="homeLogo" href="http://indigo-river.com/"><img src="<?php bloginfo('template_url');?>/img/IndigoRiver_Logo_White-01.svg"></a>
 
+<?php include('includes/siderbar_menu.php'); ?>
+
 </header>
 
 <?php include('includes/sidebar_left.php'); ?>
@@ -13,13 +15,13 @@ get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <div id="mainWrapper">
-	<div id="introHome">
+	<div id="introHome windowHeight">
     
     </div>
 </div>
 
 <?php endwhile; endif; ?>
 
-<?php include('includes/siderbar_menu.php'); ?>
+
 
 <?php get_footer(); ?>
