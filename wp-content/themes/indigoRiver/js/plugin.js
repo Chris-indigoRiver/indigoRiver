aafeb8366781f1a6b5d04668c8ef9303d4c3edd2
Jack-indigoRiver <jack@indigo-river.com>
2015-08-04 10:43:33 +0100

    menu jquery added to plugin.js

--- wp-content/themes/indigoRiver/js/plugin.js
+++ wp-content/themes/indigoRiver/js/plugin.js
@@ -8,5 +8,29 @@ $(document).ready(function(){
     $(".windowHeight").height(bodyheight);
 	
 	
+	//*  *//
 	
-})
+	$('#defaultBG').hide();
+	
+	$( ".menuIcon" ).click(function() {
+		$(this).toggleClass('on');
+		if($(this).hasClass('on')){
+			$(this).css('background' , 'transparent');
+			$(this).delay(100).animate({'width': '99%'}, 500);
+			$('.pageLocator').animate({'width': '50%'}, 200);
+			$('.nav').css({'display' : 'block'});
+			$('.appointments').css({'display' : 'block'});
+			$('.menuLogo').css({'display' : 'block'});
+			$('.locatorLine').addClass('animated fadeOut');
+		}else{
+			$(this).css('background' , '#f9f9f9');
+			$(this).animate({'width': '10%'}, 500);
+			$('.pageLocator').animate({'width': '11%'}, 200);
+			$('.nav').css('display' , 'none');
+			$('.appointments').css('display' , 'none');
+			$('.menuLogo').css('display' , 'none');
+		}
+	});
+	
+	
+});
