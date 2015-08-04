337fd931566d7754606c592a7231fa3b00c93ba2
Jack-indigoRiver <jack@indigo-river.com>
2015-08-04 10:35:09 +0100

    linking main.js in header

--- wp-content/themes/indigoRiver/header.php
+++ wp-content/themes/indigoRiver/header.php
@@ -9,9 +9,10 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/css/stylesheet.css" />
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
 <script src="//use.typekit.net/ozh5zle.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/plugin.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/main.js"></script>
+
 
 <?php wp_head(); ?>
 </head>
