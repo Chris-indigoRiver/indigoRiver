a6d9caddb19c25a4691b869f72f0755f17f26a05
Jack-indigoRiver <jack@indigo-river.com>
2015-08-05 11:26:52 +0100

    fix 4

--- wp-content/themes/indigoRiver/js/plugin.js
+++ wp-content/themes/indigoRiver/js/plugin.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 	});
 	
 	$('.ourPeople').each(function() {
-		$(this).height($(this).width() + ($(this).width() / 100 * 3);
+		$(this).height($(this).width() + $(this).width() / 100 * 3);
 	});
 
 	$(window).resize(function(){
@@ -69,7 +69,7 @@ $(document).ready(function(){
 
 	function resizeOurPeople(){
 		$('.ourPeople').each(function() {
-			$(this).height($(this).width() + ($(this).width() / 100 * 3);
+			$(this).height($(this).width() + $(this).width() / 100 * 3);
 		});
 		$(".peopleIntro").css({'height':($(".ourPeople").height())});
 		$(".joinCTA").css({'height':($(".ourPeople").height())});
