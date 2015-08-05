45690a2972f56972803d5381af8a9d63d2798bbd
Jack-indigoRiver <jack@indigo-river.com>
2015-08-05 09:59:30 +0100

    edit to if

--- wp-content/themes/indigoRiver/js/plugin.js
+++ wp-content/themes/indigoRiver/js/plugin.js
@@ -28,8 +28,9 @@ $(document).ready(function(){
 			$('.menuLogo').css({'display' : 'block'});
 			$('.locatorLine').addClass('animated fadeOut');
 			$('.homeNavLogo').addClass('animated fadeIn');
-		}if ($(window).width() <= 600){
-			$('.appointments').animate({'width' : '50%'});
+			if ($(window).width() <= 600){
+				$('.appointments').animate({'width' : '50%'});
+			}
 		}else{
 			//$('#pageMenu').css('background' , '#f9f9f9');
 			$('#pageMenu').animate({'width': '10%'}, 500);
