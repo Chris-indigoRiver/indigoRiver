0de20b032483941c80784a708f9df1b9ab127c75
Jack-indigoRiver <jack@indigo-river.com>
2015-08-06 10:19:56 +0100

    plus positioning

--- wp-content/themes/indigoRiver/footer.php
+++ wp-content/themes/indigoRiver/footer.php
@@ -14,20 +14,20 @@
 	        <p>4 Chestnut Court <br>Jill Lane <br>Sambourne <br>B96 6EW</p>
 	    </div><div class="footerDetails footerRight">
 	    	<h3>Follow</h3>
-        	<a href="https://www.facebook.com/IndigoRiverCreative"><div class="footerSM">
+        	<a TARGET="_blank" href="https://www.facebook.com/IndigoRiverCreative"><div class="footerSM">
             	<img src="http://www.indigo-river.com/wp-content/themes/html5blank-stable/img/icons/fb-01.svg"><p>Facebook</p>
             </div></a>
-            <a href="https://twitter.com/indigoriverco"><div class="footerSM">
+            <a TARGET="_blank" href="https://twitter.com/indigoriverco"><div class="footerSM">
             	<img src="http://www.indigo-river.com/wp-content/themes/html5blank-stable/img/icons/twitter-01.svg"><p>Twitter</p>
             </div></a>
-            <a href="https://instagram.com/indigo.river/"><div class="footerSM">
+            <a TARGET="_blank" href="https://instagram.com/indigo.river/"><div class="footerSM">
             	<img src="http://www.indigo-river.com/wp-content/themes/html5blank-stable/img/icons/instagram-01.svg"><p>Instagram</p>
             </div></a>
 	    </div>
 	</div>
 	<div class="contactMap2">
     <div class="background_opaque footerMessage">
-    	<a href="https://www.google.co.uk/maps/place/Indigo+River+Creative+Ltd/@52.2659908,-1.9196016,15z/data=!4m2!3m1!1s0x4870c1815e149dcf:0x37f4ce091cf81e35?hl=en"><h2 class="" style="font-family: 'bebas_neue'; font-size:50px;  padding-top: 12%;">Come and find <span style="border-bottom:5px solid;">us</span>
+    	<a TARGET="_blank" href="https://www.google.co.uk/maps/place/Indigo+River+Creative+Ltd/@52.2659908,-1.9196016,15z/data=!4m2!3m1!1s0x4870c1815e149dcf:0x37f4ce091cf81e35?hl=en"><h2 class="" style="font-family: 'bebas_neue'; font-size:50px;  padding-top: 12%;">Come and find <span style="border-bottom:5px solid;">us</span>
         </h2></a>
         <p class="white">Feel free to drop into our HQ for a cuppa and a biscuit, we'll have one ready - chocolate digestive or custard cream?</p>
 	</div>
