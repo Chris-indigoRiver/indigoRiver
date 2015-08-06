7b3d5d527481f65ae501ba3a293bc2962787926e
Jack-indigoRiver <jack@indigo-river.com>
2015-08-06 15:32:00 +0100

    new meta

--- wp-content/themes/indigoRiver/header.php
+++ wp-content/themes/indigoRiver/header.php
@@ -3,8 +3,9 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
-<meta name="viewport" content="width=device-width, initial-scale=0.8, minimum-scale=0.8, user-scalable=no">
+<meta name="viewport" content="width=device-width, initial-scale=0.9, minimum-scale=0.9, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="format-detection" content="telephone=no">
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
