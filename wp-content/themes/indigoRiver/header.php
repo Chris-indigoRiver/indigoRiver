ac5c966fabe1dc45c0aaa4bd200c9dbc2700cc0d
Jack-indigoRiver <jack@indigo-river.com>
2015-08-06 15:25:27 +0100

    new metas TEST

--- wp-content/themes/indigoRiver/header.php
+++ wp-content/themes/indigoRiver/header.php
@@ -2,7 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">-->
+<meta name="viewport" content="width=device-width, initial-scale=0.8, minimum-scale=0.8, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
