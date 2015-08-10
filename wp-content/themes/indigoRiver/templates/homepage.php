96b55495e4d076c6a44c62b0ba75224b5ac48aea
Jack-indigoRiver <jack@indigo-river.com>
2015-08-10 10:34:50 +0100

    header button

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -4,7 +4,6 @@ Template Name: Home Page
 get_header(); ?>
 
 <header>
-<a class="homeLogo" href="http://indigo-river.com/"><img src="<?php bloginfo('template_url');?>/img/IndigoRiver_Logo_White-01.svg"></a>
 </header>
 <?php include('includes/siderbar_menu.php'); ?>
 <?php include('includes/sidebar_left.php'); ?>
@@ -15,6 +14,7 @@ get_header(); ?>
 	<article id="introHome" class="windowHeight">
         <div class="background_opaque_header">
         </div>
+        <a class="homeLogo" href="http://indigo-river.com/"><img src="<?php bloginfo('template_url');?>/img/IndigoRiver_Logo_White-01.svg"></a>
     	<section class="cd-intro animated fadeIn">
         	<h1 class="cd-headline letters type">
                 <span>With Us<br/>it's always<br/></span>
