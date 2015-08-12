b51c86a2ff049390bf39ba8f5014ace0f1b9a9dd
Jack-indigoRiver <jack@indigo-river.com>
2015-08-12 15:14:34 +0100

    menu minus img

--- wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
+++ wp-content/themes/indigoRiver/templates/includes/siderbar_menu.php
@@ -34,7 +34,7 @@
                             <h2 class="navNewsTitle"><?php echo the_title(); ?></h2>
                             <p class="navNewsExcerpt"><?php echo get_field("custom_excerpt"); ?></p>
                             <?php if(get_field("logo_field")) { ?>
-                            <img class="navNewsLogo" src="<?php echo get_field("logo_field"); ?>" alt="indigoRiver Creative marketing agency <?php echo the_title(); ?>" />
+                            <!--<img class="navNewsLogo" src="<?php echo get_field("logo_field"); ?>" alt="indigoRiver Creative marketing agency <?php echo the_title(); ?>" />-->
                             <?php } ?>
                             <?php 
                                 if ( has_post_thumbnail() ) { 
