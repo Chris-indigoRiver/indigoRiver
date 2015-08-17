4840bd62387c82b799714b6888c08e25b2df6950
Jack-indigoRiver <jack@indigo-river.com>
2015-08-17 09:59:44 +0100

    pagination php 3

--- wp-content/themes/indigoRiver/templates/includes/sidebar_left.php
+++ wp-content/themes/indigoRiver/templates/includes/sidebar_left.php
@@ -6,13 +6,16 @@
 	</div>
 	<div class="pagination">
 		<?php $slug = get_the_title(); 
-			echo $slug;
 			if ($slug == "Home") { ?>
 	        <div class="locatorLine animated slideInRight" id="LLOne" style="background: rgb(0, 0, 0);"></div>
 	        <div class="locatorLine animated slideInRight" id="LLTwo" style="background: rgb(204, 204, 204);"></div>
 	        <div class="locatorLine animated slideInRight" id="LLThree" style="background: rgb(204, 204, 204);"></div>
 	        <div class="locatorLine animated slideInRight" id="LLFour" style="background: rgb(204, 204, 204);"></div>
 			<?php }
+			else if ($slug == "Work") {?>
+			<div class="locatorLine animated slideInRight" id="LLOne" style="background: rgb(0, 0, 0);"></div>
+	        <div class="locatorLine animated slideInRight" id="LLTwo" style="background: rgb(204, 204, 204);"></div>
+			<?php}
 		?>
 	</div>
 </div>
