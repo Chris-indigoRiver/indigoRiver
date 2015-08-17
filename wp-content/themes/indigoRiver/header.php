ee256e3d01bc84c9da11966ba542ef3ed9d98bca
Christian <chris@indigo-river.com>
2015-08-17 17:17:16 +0100

    reposition js

--- wp-content/themes/indigoRiver/header.php
+++ wp-content/themes/indigoRiver/header.php
@@ -10,10 +10,10 @@
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquery.bxslider.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/plugin.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/center/jquery.blImageCenter.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/headline_js/main.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/js/jquery.vibe.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/js/plugin.js"></script>
 <script src="//use.typekit.net/ozh5zle.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
 <script src="//use.typekit.net/fge5ldw.js"></script>
