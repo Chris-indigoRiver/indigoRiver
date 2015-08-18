f934a8b7a563c79d60af15eea2d2416c64f14fe0
Christian <chris@indigo-river.com>
2015-08-18 21:52:16 +0100

    removed controls from video

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -14,7 +14,7 @@ get_header(); ?>
 <div id="mainWrapper">
 	<article id="introHome" class="windowHeight">
         <div class="mainSlider" class="windowHeight">
-            <video id="videoBG" controls preload autoplay>
+            <video id="videoBG" preload autoplay>
               <source src="<?php bloginfo('template_url');?>/media/video_bg.mp4" type="video/mp4">
               
               Your browser does not support the video tag.
