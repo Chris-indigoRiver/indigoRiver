12218931d48534a3834e23d39e6411651acecbdb
Jack-indigoRiver <jack@indigo-river.com>
2015-10-02 12:54:01 +0100

    square class

--- wp-content/themes/indigoRiver/js/main.js
+++ wp-content/themes/indigoRiver/js/main.js
@@ -283,7 +283,8 @@ $(window).resize(function(){
 			$('.square').height(divWidth);
 		});	
 
-		$('.navNewsWrapper .bx-prev').addClass("square");	
+		$('.navNewsWrapper .bx-prev').addClass("square");
+		$('.navNewsWrapper .bx-next').addClass("square");	
 		
 		$('.beliefImg').each(function() {
 			$(this).height($(this).width() + 50);
