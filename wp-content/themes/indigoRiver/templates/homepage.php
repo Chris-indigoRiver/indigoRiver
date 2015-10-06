01705e1ac4dd6eed176f973f47aba67fcd0d256d
Jack-indigoRiver <jack@indigo-river.com>
2015-10-06 14:21:58 +0100

    services titles on slide

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -160,7 +160,7 @@ get_header(); ?>
 								} 
 							?>
                             <div class="homeSlideTitle">
-                                <?php echo '<h2>' the_title(); echo '</h2>' ?></h2>
+                                <?php echo the_title(); ?></h2>
                             </div>
                         </div>
                         <div class="sliderText" id="">
