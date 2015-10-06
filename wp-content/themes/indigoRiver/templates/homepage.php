fca34957a2badfc20c51e39c962bbc5e940428ac
Jack-indigoRiver <jack@indigo-river.com>
2015-10-06 14:49:41 +0100

    border in slides

--- wp-content/themes/indigoRiver/templates/homepage.php
+++ wp-content/themes/indigoRiver/templates/homepage.php
@@ -170,6 +170,8 @@ get_header(); ?>
                                 </div>
                             </div>
                         </div>
+                        <div class="fcBorder">
+                        </div>
                     </div>
               	</li>
                 <?php endwhile; wp_reset_query(); ?>
